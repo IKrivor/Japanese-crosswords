cd93704aedec00ffad23faa4978dfcc83ac3d3b6
Криворотова Ирина <kin2805@yandex.ru>
2016-05-03 02:48:58 +0600

    Разбить сайт на блоки

---
+++ www/blocks/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div id="rights">
+        Все права защищены &copy <?php echo date('Y');?>
+    </div>
+</footer>
--- www/index.php
+++ www/index.php
@@ -1,25 +1,14 @@
+<!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Японские кроссворды</title>
-    <link href="/css/style.css" rel="stylesheet" type="text/css">
+    <?php 
+    $title = "Кроссворды";
+    require_once "blocks/head.php"; 
+    ?>
 </head>
 <body>
-    <header>
-        <ul id="mainMenu">
-            <li><a href="index.php">Кроссворды</a></li>
-            <li><a href="rules.php">Правила</a></li>
-        </ul>
-        <ul id="regAuth">
-            <li><a>Войти</a></li>
-            <li><a>Регистрация</a></li>
-        </ul>
-    </header>
+    <?php require_once "blocks/header.php" ?>
 
-    <footer>
-        <div id="rights">
-            Все права защищены &copy <?php echo date('Y');?>
-        </div>
-    </footer>
+    <?php require_once "blocks/footer.php" ?>
 </body>
 </html>
