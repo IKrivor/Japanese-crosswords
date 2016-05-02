0485e5ff150e114ed00855cc1b1edf981b0dffe8
Криворотова Ирина <kin2805@yandex.ru>
2016-05-03 02:23:10 +0600

    Добавить шапку

--- www/index.php
+++ www/index.php
@@ -5,5 +5,15 @@
     <link href="/css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+    <header>
+        <ul id="mainMenu">
+            <li><a href="index.php">Кроссворды</a></li>
+            <li><a href="rules.php">Правила</a></li>
+        </ul>
+        <ul id="regAuth">
+            <li><a>Войти</a></li>
+            <li><a>Регистрация</a></li>
+        </ul>
+    </header>
 </body>
 </html>
