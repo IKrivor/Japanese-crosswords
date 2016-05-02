969738e5846d1499ad41b1fbc04a7b2b1244f5a4
Криворотова Ирина <kin2805@yandex.ru>
2016-05-03 03:10:41 +0600

    Добавить основную часть на главную страницу

--- www/index.php
+++ www/index.php
@@ -8,6 +8,18 @@
 </head>
 <body>
     <?php require_once "blocks/header.php" ?>
+    
+    <div id="body">
+        <div class="cross">
+            <a href="cross.php"><img src="img/cross_test.png"></a>
+        </div>
+        <div class="cross">
+            <a href="cross.php"><img src="img/cross_test.png"></a>
+        </div>
+        <div class="cross">
+            <a href="cross.php"><img src="img/cross_test.png"></a>
+        </div>
+    </div>    
 
     <?php require_once "blocks/footer.php" ?>
 </body>
