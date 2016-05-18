879ff6b44aebda9e7554c4ac17ab537b98f98719
Криворотова Ирина <kin2805@yandex.ru>
2016-05-19 02:30:48 +0600

    Добавить лого, изменить картинки фонового слайдера

--- www/blocks/head.php
+++ www/blocks/head.php
@@ -1,3 +1,37 @@
+<?php
+require_once "./functions/crossDB.php";
+?>
 <meta charset="utf-8">
 <title><?=$title?></title>
 <link href="/css/style.css" rel="stylesheet" type="text/css">
+<link href="css/bgslider.css" rel="stylesheet" type="text/css" />
+
+<script type="text/javascript" src="js/jquery-1.5.2.min.js"></script>
+<script type="text/javascript" src="js/bgstretcher.js"></script>
+
+<script type="text/javascript">
+
+    $(document).ready(function(){
+
+        //  Initialize Backgound Stretcher
+        $('BODY').bgStretcher({
+            images: ['img/fon/fon_1.jpg', 'img/fon/fon_2.jpg', 'img/fon/fon_3.jpg', 'img/fon/fon_4.jpg', 'img/fon/fon_5.jpg',
+                'img/fon/fon_6.jpg'],
+            imageWidth: 1024,
+            imageHeight: 768,
+            callbackfunction: $(this).bgStretcher.pause(),
+            slideDirection: 'W',
+            slideShowSpeed: 1000,
+            transitionEffect: 'fade',
+            sequenceMode: 'normal',
+            buttonPrev: '#prev',
+            buttonNext: '#next',
+            pagination: '#nav',
+            anchoring: 'left center',
+            anchoringImg: 'left center'
+
+        });
+
+    });
+
+</script>
--- www/blocks/header.php
+++ www/blocks/header.php
@@ -1,10 +1,22 @@
 <header>
-    <ul id="mainMenu">
-        <li><a href="/index.php">Кроссворды</a></li>
-        <li><a href="/rules.php">Правила</a></li>
-    </ul>
-    <ul id="regAuth">
-        <li><a>Войти</a></li>
-        <li><a>Регистрация</a></li>
-    </ul>
+    <a href="#" id='prev'><img src="./img/left-arrow.png"></a>  <a href="#" id='next'>
+        <img src="./img/right-arrow.png"></a>
+    
+    <div id="logo"><img src="./img/logo.png"></div>
+    
+    <div id="underLogo">
+        <ul id="mainMenu">
+            <li><a href="/index.php">Кроссворды</a></li>
+            <li><a href="/rules.php">Правила</a></li>
+        </ul>
+        <ul id="regAuth">
+            <li><a>Войти</a></li>
+            <li><a>Регистрация</a></li>
+        </ul>
+    </div>
+    
+
+    <div id="page" style="display: none_;"></div>
+
+    
 </header>
